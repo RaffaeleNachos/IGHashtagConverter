d2be2a47b551c611731068ffddcfee1c16c864fd
Raffaele <r.apetino@studenti.unipi.it>
2017-09-08 21:18:20 +0200

    Updated!

--- hashtagbotconv.c
+++ hashtagbotconv.c
@@ -2,16 +2,17 @@
 #include <stdlib.h>
 #include <string.h>
 
-void ins_cancelletto(char **a, char *b)
+void ins_hashtag(char **a, char *b)
 {
     int i=0;
     int j;
-    printf("Inserire i tag seguiti dalla parola 'stop' senza apici:\n");
+    printf("* Insert the words followed by the word 'stop' without quotes *\n");
     scanf("%s", a[i]);
     while(strcmp(a[i],b)!=0){
         i++;
         scanf("%s", a[i]);
     }
+	printf("* Here them are! Just copy them! *\n");
     for(j=0; j<i; j++){
         printf("#%s ", a[j]);
     }
@@ -21,36 +22,67 @@ void ins_cancelletto(char **a, char *b)
 void format_bot(char **a, char *b)
 {
     int i=0;
-    int j;
-    printf("Inserire i tag seguiti dalla parola 'stop' senza apici:\n");
-    scanf("%s", a[i]);
-    while(strcmp(a[i],b)!=0){
-        i++;
-        scanf("%s", a[i]);
-    }
-    for(j=0; j<i; j++){
-        printf("'%s', ", a[j]);
-    }
-    printf("\n");
+    int j,k,x;
+	printf("** Insert 1 if you are inserting WITH hashtag '#' (example: #hello #red #yellow) **\n");
+	printf("** Insert 2 if you are inserting WITHOUT hashtag '#' (example: hello red yellow) **\n");
+	scanf("%d", &x);
+	if (x==1){
+		printf("* Insert the tags followed by the word 'stop' without quotes (example: #hello #red #yellow stop) *\n");
+    	scanf("%s", a[i]);
+    	while(strcmp(a[i],b)!=0){
+       		i++;
+        	scanf("%s", a[i]);
+   		}
+		printf("* Here them are! Just copy them! *\n");
+		for(j=0; j<i; j++){
+			printf("'");
+        	for(k=1; a[j][k]!=0; k++){
+        	    printf("%c", a[j][k]);
+       		 }
+		printf("',");
+      	printf(" ");
+    	}
+	}
+	else if (x==2){
+		printf("* Insert the tags followed by the word 'stop' without quotes (example: #hello #red #yellow stop) *\n");
+    	scanf("%s", a[i]);
+    	while(strcmp(a[i],b)!=0){
+       		i++;
+        	scanf("%s", a[i]);
+   		}
+	    for(j=0; j<i; j++){
+ 	       printf("'%s', ", a[j]);
+	    }
+	}
+	else {
+		printf("* You did not insert the right number, restart the program *\n");
+	}
+	printf("\n");
 }
 
-void del_cancelletto(char **a, char *b)
+void del_hashtag(char **a, char *b)
 {
     int i=0;
     int k,j;
-    printf("Inserire i tag seguiti dalla parola 'stop' senza apici:\n");
+    printf("* Insert the tags followed by the word 'stop' without quotes *\n");
     scanf("%s", a[i]);
     while(strcmp(a[i],b)!=0){
         i++;
         scanf("%s", a[i]);
     }
+	printf("* Here them are! Just copy them! *\n");
     for(j=0; j<i; j++){
         for(k=1; a[j][k]!=0; k++){
             printf("%c", a[j][k]);
         }
-        printf(" ");
     }
     printf("\n");
+}
+
+void printbesthashtags()
+{
+	printf("* Here them are! Just copy them! *\n");
+	printf("'love', 'look', 'selfie', 'winter', 'tweegram', 'photooftheday', '20likes', 'amazing', 'smile', 'follow4follow', 'like4like', 'heroscommunity', 'instalike', 'igers', 'picoftheday', 'food', 'instadaily', 'instafollow', 'me', 'girl', 'iphoneonly', 'instagood', 'bestoftheday', 'instacool', 'instago', 'all_shots', 'follow', 'webstagram', 'colorful', 'style', 'swag', 'cute'\n");
 }
 
 int main ()
@@ -63,19 +95,25 @@ int main ()
     for(i=0; i<1000; i++){
         a[i]=malloc(1001*sizeof(char));
     }
-    printf("Inserisci 1 per aggiungere il cancelletto prima di ogni hashtag\n");
-    printf("Inserisci 2 per formattare gli hashtag per instabot\n");
-    printf("Inserisci 3 per stampare gli hashtag senza cancelletto\n");
+    printf("* Insert 1 for adding the hashtag '#' before every word (useful for posting on instagram)                     *\n");
+    printf("* Insert 2 for getting your hashtag ready for instabot (insert them without the hashtag, use firstly mode 3!) *\n");
+    printf("* Insert 3 for printing the hashtags without the '#'                                                          *\n");
+	printf("* Insert 4 for printing the best 20 hashtags out there! (they are ready for instabot)                         *\n");
     scanf("%d", &scelta);
     if (scelta==1){
-        ins_cancelletto(a,b);
+        ins_hashtag(a,b);
     }
-
-    if (scelta==2){
+    else if (scelta==2){
         format_bot(a,b);
     }
-    if (scelta==3){
-        del_cancelletto(a,b);
+    else if (scelta==3){
+        del_hashtag(a,b);
     }
+	else if (scelta==4){
+        printbesthashtags();
+    }
+	else {
+		printf("* You did not insert the right number, restart the program *\n");
+	}
     return 0;
 }
